c309adab4d81f872e396d1af2614f70610594256
Dong <dong0125@vt.edu>
2015-11-28 02:37:16 -0500

    basic grid

---
+++ snake.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from Tkinter import *
+import sys
+from random import randint
+
+class Grid(object):
+    def __init__ (self,master=None,width=1000,height=1000,grid_width=10,offset=20): 
+	self.height = height
+	self.width = width
+	self.grid_x = width / grid_width 
+	self.grid_y = height / grid_height 
+
+
+
+
+#if __name__ == "__main__":
+
